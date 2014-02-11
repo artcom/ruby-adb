46c8889c6540f23888fdfc1108fbe44d42bba361
Guido Lorenz <guido.lorenz@artcom.de>
2014-02-11 11:42:50 +0100

    Mock system calls in spec

--- spec/adb/adb_spec.rb
+++ spec/adb/adb_spec.rb
@@ -5,6 +5,7 @@ describe Adb::Wrapper do
   subject(:adb) { Adb::Wrapper.new }
 
   it 'returns the version' do
-    expect(adb.version).to eq('1.0.31')
+    allow(adb).to receive(:`).with('adb version').and_return("Android Debug Bridge version 23.42.1\n")
+    expect(adb.version).to eq('23.42.1')
   end
 end
